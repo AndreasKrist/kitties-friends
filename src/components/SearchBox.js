353be5c104d5def581a8c1f6cf00c500dac1a0af
Andreas Kristianto <andreaskrist2004@gmail.com>
2024-12-01 00:37:59 +0700

    update kitten

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,7 +6,7 @@ const SearchBox = ({searchChange}) => {
             <input 
             className="pa3 ba b--blue bg-lightest-blue"
             type="search" 
-            placeholder="search robots"
+            placeholder="search kittie"
             onChange={searchChange}
             />
         </div>
